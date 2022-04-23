4b3aae88f1031cffc884b3ef6e98f56248766a97
Marximus Maximus <94208961+MarximusMaximus@users.noreply.github.com>
2022-04-22 17:34:14 -0700

    Add linter flake8 (#17)
    fixes #7 
    
    * Generate pretty html reports of test results
    
    * add pre-commit as dev dependency
    
    * add a BUNCH of linters
    
    * clarify not supporting python4 so that we can install other packages that explicitly do not support python4
    
    * Add pre-commit badge to README.rst
    
    * Add lots of linters (but not activated) that we want to explore using
    
    * update spelling dict
    
    * flake8 setup & compatibility settings for black and mypy
    
    * updates to files based on flake8 and mypy

--- src/pytest_prefer_nested_dup_tests/__init__.py
+++ src/pytest_prefer_nested_dup_tests/__init__.py
@@ -1,1 +1,12 @@
-from .__impl import *
+"""
+pytest-prefer-nested-dup-tests
+
+by Marximus Maximus (https://www.marximus.com)
+
+A Pytest plugin to drop duplicated tests during collection, but will prefer
+keeping nested packages.
+
+By default, when de-duplicating tests, all sub-packages become top level packages.
+This plugin keeps the subpackage structure intact.
+"""  # noqa: D400
+from .__impl import *  # noqa: F401, F403
